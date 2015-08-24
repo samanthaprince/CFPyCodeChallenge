a75b66e5a4aa1d81ee81d769252566a91c87e58a
Samantha Prince <samanthadebella@Samanthas-MacBook-Pro.local>
2015-08-24 12:42:59 -0700

    Added library, shelf and book objects, still working

---
+++ question1/library.py
@@ -0,0 +1,19 @@
+class Library(object):
+    """Create a new library object"""
+    def __init__(self):
+        self.shelves = []
+        self.books = []
+
+
+class Shelf():
+    """Create a shelf within the Library"""
+    def __init__():
+
+
+
+
+class Book():
+    """Creates a new book including book title and author"""
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
