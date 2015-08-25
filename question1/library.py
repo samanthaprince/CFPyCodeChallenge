20fe8f2b727048beb818575edaddd1be66de5443
Samantha Prince <samanthadebella@Samanthas-MacBook-Pro.local>
2015-08-25 12:31:57 -0700

    Added functions to library object, edited shelf and book objects

--- question1/library.py
+++ question1/library.py
@@ -4,15 +4,22 @@ class Library(object):
         self.shelves = []
         self.books = []
 
+    def add_shelf(self, shelf):
+        """Add shelf to library"""
+        self.shelves.append(shelf)
 
-class Shelf():
+    def return_to_shelf(self, shelf, book):
+        """returns a book to a shelf"""
+        shelf.books.append(book)
+
+
+class Shelf(object):
     """Create a shelf within the Library"""
-    def __init__():
+    def __init__(self, name):
+        self.name = name
 
 
-
-
-class Book():
+class Book(object):
     """Creates a new book including book title and author"""
     def __init__(self, title, author):
         self.title = title
